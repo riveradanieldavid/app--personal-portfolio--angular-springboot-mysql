4d5963697a28946460e8813f6905ddc59fab7542
riveradanieldavid <dannieldavvid2004@gmail.com>
2022-08-13 10:56:14 -0300

    Rivera Daniel > Portfolio > S: Routing básico aplicado...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// THIS  "import" IMPLEMENTS THE CHILDS COMPONENTS OF "app.comoponent.html"
+import { RoutingComponent } from './components/home/routing/routing.component';
+// THE COMPONENT ON TOP ALREADY CONTAINS ALL CHILDS OF "app.comoponent.html"
+
+// "routes" CONST USES "Routes" FROM "@angular/router"
+// MAKES VISIBLE "routing.component.html" FILE  WHICH CONTAINS  COMPONENTS OF VIEWS
+const routes: Routes = [
+  { path: '', component: RoutingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// NEW MODULES MUST BE DECLARED HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+import { HomeModule } from "./components/home/home.module";
+import { GeneralModule } from "./components/general/general.module";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // NEW MODULES MUST BE DECLARED HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
+    GeneralModule,
+    HomeModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
