a1dc79252db7df5e8d9a6f027ade01be3864021d
riveradanieldavid <dannieldavvid2004@gmail.com>
2022-09-19 12:18:45 -0300

    Rivera Daniel > Portfolio > S: Estilos tarjeta experiencia modo público, edición.

--- frontend/src/app/components/home/article-general-experience/article-text/article-text.component.ts
+++ frontend/src/app/components/home/article-general-experience/article-text/article-text.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { About } from 'src/app/_models/about.model';
+import { AboutService } from 'src/app/_services/about.service';
+// ADDED
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
+// ADDED /
 
 @Component({
   selector: 'app-article-text',
@@ -6,10 +11,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-text.component.css']
 })
 export class ArticleTextComponent implements OnInit {
+  // ATTRIBUTES
+  about?: About[];
+  ccccurrentAbout: About = {};
+  currentIndex = -1;
+  title = '';
+  // ADDED
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
+  // ADDED /
 
-  constructor() { }
+  // CONSTRUCTOR
+  constructor(
+    private aboutService: AboutService,
+    // ADDED
+    private tokenStorageService: TokenStorageService
+  ) // ADDED /
+  { }
 
+  // DATA AVAILABLE
   ngOnInit(): void {
+    this.retrieveAbout();
+    // ADDED
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+      this.username = user.username;
+    }
+    // ADDED /
+  }
+  // GET DATA FROM SERVICES TO BE AVAILABLE IN THE HTML FILE
+  retrieveAbout(): void {
+    this.aboutService.getAll().subscribe({
+      next: (data) => {
+        this.about = data;
+        console.log(data);
+      },
+      error: (e) => console.error(e),
+    });
+  }
+
+  refreshList(): void {
+    this.retrieveAbout();
+    this.ccccurrentAbout = {};
+    this.currentIndex = -1;
+  }
+  // SHOW ARTICE TO EDIT
+  setActiveAbout(about: About, index: number): void {
+    this.ccccurrentAbout = about;
+    this.currentIndex = index;
   }
 
 }
